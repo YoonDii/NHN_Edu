40d29f324c909f96b917678aab5fb8b6093b470c
YoonDii <dbswl9506@naver.com>
2023-01-22 19:21:42 +0900

    feat:view

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Searchlist(models.Model):
+    url = models.CharField(max_length=500)
+    title = models.CharField(max_length = 200)
+    published_datetime = models.DateTimeField(auto_now_add = True)
+    boby = models.CharField(max_length=500)
+    attachment_list = models.CharField(max_length=500)
+
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
+import json
+import requests
+
 from django.shortcuts import render
+from .models import Searchlist
+from django.http import HttpRequest , JsonResponse
+from bs4 import BeautifulSoup
 
 # Create your views here.
 
 def index(request):
-    pass
-render 
+
+    return render (request,"blog/index.html")
